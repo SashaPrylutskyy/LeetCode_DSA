f3e5dff8f3ff3ec0e5de2b152003ee8f38d19bda
Oleksandr <62639481+SashaPrylutskyy@users.noreply.github.com>
2025-08-29 09:31:12 +0300

    1580-SHUFFLE-THE-ARRAY Stats: Time: 0 ms (100%), Space: 45 MB (25.47%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -2,10 +2,9 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
        int[] shuffled = new int[nums.length];
        int left = 0;
-       int right = n;
 
        for (int i = 0; i < nums.length; i++) {
-        shuffled[i] = (i % 2 == 0) ? nums[left++] : nums[right++];
+        shuffled[i] = (i % 2 == 0) ? nums[left++] : nums[n++];
        }
        return shuffled;
     }
